ccf5d001e95eb7740be306258e29f2d1f1104af8
Jorge Lucas <jorge.tomasgt@gmail.com>
2024-10-10 10:59:58 +0100

    start

---
+++ src/main/kotlin/Reviewing_Kotlin_data_structures.kt
@@ -0,0 +1,10 @@
+
+fun reviewingKotlinDatastructures(){
+    //lists, sets, and maps.
+    // ==== lists
+    //val hobbits = listOf("Frodo", "Sam", "Pippin", "Merry")
+    //val hobbits: List<String> = listOf("Frodo", "Sam", "Pippin", "Merry")
+
+    // ==== Sets
+
+}
